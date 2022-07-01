4cda87e83fc514f9381abda607de7426e0aee83c
Rémi Bardon <remi.bardon.dev@gmail.com>
2022-07-01 21:59:53 +0200

    🧑‍💻 Add supported `platforms` and cleanup `Package.swift`

--- Package.swift
+++ Package.swift
@@ -5,20 +5,15 @@ import PackageDescription
 
 let package = Package(
 	name: "prefixed",
+	platforms: [
+		.macOS(.v10_13),
+		.iOS(.v11),
+	],
 	products: [
-		// Products define the executables and libraries a package produces, and make them visible to other packages.
-		.library(
-			name: "Prefixed",
-			targets: ["Prefixed"]
-		),
+		.library(name: "Prefixed", targets: ["Prefixed"]),
 	],
 	targets: [
-		// Targets are the basic building blocks of a package. A target can define a module or a test suite.
-		// Targets can depend on other targets in this package, and on products in packages this package depends on.
 		.target(name: "Prefixed"),
-		.testTarget(
-			name: "PrefixedTests",
-			dependencies: ["Prefixed"]
-		),
+		.testTarget(name: "PrefixedTests", dependencies: ["Prefixed"]),
 	]
 )
